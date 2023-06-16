87499a2d92dfb8c40b6733620f013a8412261557
Lucas da Silva <lucassouza00012345@gmail.com>
2023-06-16 11:15:10 -0300

    feat: implement Title and Pokemons in App.tsx

--- App.tsx
+++ App.tsx
@@ -1,21 +1,41 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import Pokemons from './src/components/Pokemons'
 
 export default function App() {
-	return (
-		<View style={styles.container}>
-			<Text>Open up App.tsx to start working on your app!</Text>
-			<StatusBar style="auto" />
-		</View>
-	);
+  return (
+    <View style={styles.container}>
+      <View style={styles.containerTitle}>
+        <Text style={styles.title}>Pokédex</Text>
+        <Text style={styles.subtitle}>
+          Pesquise um Pokémon pelo nome ou usando seu número National Pokédex.
+        </Text>
+      </View>
+      <Pokemons />
+      <StatusBar style="auto" />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-});
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    paddingHorizontal: 12,
+    marginTop: 60,
+  },
+  containerTitle: {
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 32,
+    color: '#2d2f58',
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  subtitle: {
+    color: '#656a81',
+  },
+})
