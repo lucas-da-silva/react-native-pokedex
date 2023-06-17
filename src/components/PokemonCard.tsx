bc550f87f9914b2682bf52c0670cbcae13e76945
Lucas da Silva <lucassouza00012345@gmail.com>
2023-06-17 12:03:48 -0300

    feat: adds new pokemon in TypesPokemons Enum and style for PokemonCard component

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -2,33 +2,84 @@ import React from 'react'
 import {
   View, Text, Image, StyleSheet,
 } from 'react-native'
-import { IPokemonCard } from '../interfaces/Pokemon'
+import type { IPokemonCard } from '../interfaces'
+import { TypesPokemons } from '../utils'
 
-export default function PokemonCard({ name, id, uri }: IPokemonCard) {
+export default function PokemonCard({
+  name, id, uri, types,
+}: IPokemonCard) {
   return (
-    <View style={styles.container}>
-      <Image
-        source={{
-          uri,
-        }}
-        style={styles.image}
-      />
-      <Text>{name}</Text>
-      <Text>{`#00${id}`}</Text>
+    <View style={[styles.container, { backgroundColor: TypesPokemons[types[0]].color }]}>
+      <View style={styles.containerId}>
+        <Text style={styles.id}>{id.toString().padStart(3, '0')}</Text>
+      </View>
+      <View style={styles.containerImage}>
+        <Image
+          source={{
+            uri,
+          }}
+          style={styles.image}
+        />
+      </View>
+      <Text style={styles.name}>{name.charAt(0).toUpperCase() + name.slice(1)}</Text>
+      <View style={styles.containerTypes}>
+        {
+          types.map((type) => (
+            <View key={type} style={styles.containerType}>
+              <Text style={styles.type}>{type}</Text>
+            </View>
+          ))
+        }
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'gray',
     marginBottom: 15,
-    borderRadius: 20,
-    width: 185,
-    height: 150,
+    marginHorizontal: 7.5,
+    borderRadius: 15,
+    width: 140,
+    height: 210,
+    alignItems: 'center',
+  },
+  containerId: {
+    alignSelf: 'flex-end',
+    marginRight: 7,
+    marginTop: 2,
+  },
+  id: {
+    fontSize: 13,
+    color: '#656a81',
+    fontWeight: '500',
+  },
+  containerImage: {
+    marginTop: 7,
+    marginBottom: 14,
   },
   image: {
     width: 100,
     height: 100,
+  },
+  name: {
+    color: '#2d2f58',
+    fontWeight: 'bold',
+    marginBottom: 2,
+    fontSize: 17,
+  },
+  containerTypes: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 2,
+  },
+  containerType: {
+    width: 70,
+    alignItems: 'center',
+    height: 30,
+  },
+  type: {
+    fontSize: 13,
+    color: '#2d2f58',
   },
 })
--- src/utils/TypesPokemons.ts
+++ src/utils/TypesPokemons.ts
@@ -1,8 +1,22 @@
-enum TypesPokemons {
-  grass = '#49d0b0',
-  fire = '#fb6d6c',
-  water = '#76bdfd',
-  electric = '#ffd86f',
+const TypesPokemons = {
+  grass: { color: '#c4e3d4' },
+  fire: { color: '#ebbcb4' },
+  water: { color: '#81bfff' },
+  electric: { color: '#ffde7c' },
+  normal: { color: '#dbd6d6' },
+  ice: { color: '#a1d9e6' },
+  fighting: { color: '#e6c0a1' },
+  poison: { color: '#c99cd0' },
+  ground: { color: '#d4c4a0' },
+  flying: { color: '#a1b6e6' },
+  psychic: { color: '#ff94cc' },
+  bug: { color: '#c8d07c' },
+  rock: { color: '#b5a78d' },
+  ghost: { color: '#c7a7d6' },
+  dragon: { color: '#a07cff' },
+  dark: { color: '#9b9b9b' },
+  steel: { color: '#b1b1c3' },
+  fairy: { color: '#ffabec' },
 }
 
 export default TypesPokemons
